9316cb7538a12f37928b59d4808d79f18868bfc3
Atish Jain <atish_jain@yahoo.com>
2021-04-16 21:02:55 +0530

    Add files via upload

---
+++ VarDemo.java
@@ -0,0 +1,10 @@
+class VarDemo
+{
+	public static void main(String[] args) 
+	{
+		int x;  // variable in java are not allowed without initialization
+		        // *** this Program will not work
+
+		System.out.println("x is:"+x);
+	}
+}
